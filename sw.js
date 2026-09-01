176b8dea5040e259fdf26ad2576a8fa0575dcfe1
phoneyjung <phoneyjung@gmail.com>
2026-09-02 02:28:16 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PLP Finance · Service Worker */
-const CACHE = 'plp-finance-v5';
+const CACHE = 'plp-finance-v6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', function (e) {
